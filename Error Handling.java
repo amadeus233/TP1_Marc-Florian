06c5c694345038f32b9f78db5e49db1590de8fac
Kaleidoscope <66519460+amadeus233@users.noreply.github.com>
2024-03-19 16:10:26 +0800

    Error Handling.java

--- Error Handling.java
+++ Error Handling.java
@@ -1,10 +1,41 @@
-package dividebyzero;
+//throw 用于实际抛出一个特定的异常。
+//throws 用于在方法声明中指示该方法可能抛出的异常类型，告知方法的调用者需要处理这些异常。
+//throw is used to explicitly throw an exception.
+//throws is used in a method signature to declare that a method may throw certain exceptions.
+package yourOwnException;
+public class Main {
+
+    public static void f() throws MyException {
+        System.out.println("Throwing MyException from f()");
+        throw new MyException();
+    }
+
+    public static void g() throws MyException {
+        System.out.println("Throwing MyException from g()");
+        throw new MyException("Originated in g()");
+    }
+
+    public static void main(String[] args) {
+        try {
+            f();
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
+        try {
+            g();
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
+    }
+}
+/*The second version
+/*package dividebyzero;
 public class Main {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+/*  public static void main(String[] args) {
         try {
             System.out.println(3/0);
         } catch (ArithmeticException e) {
@@ -13,6 +44,7 @@ public class Main {
     }
 
 }
+
 /*!!!!!   The original version of the code !!!!!!!!!
 /*package dividebyzero;
 public class Main {
